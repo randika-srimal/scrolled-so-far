204e9c3ee887812d633b1e985705f880c517df0b
Randika Srimal <randika@thesanmark.com>
2017-01-04 23:52:42 +0530

    Real world heights added, need to save it in localhost

--- inject.js
+++ inject.js
@@ -16,20 +16,36 @@ var readyStateCheckInterval = setInterval(function () {
 
             var dpi_y = document.getElementById('dpi-element').offsetHeight;
 
+            var indicatorWrapper = document.createElement('div');
+            indicatorWrapper.style.right = '32px';
+            indicatorWrapper.style.top = '2px';
+            indicatorWrapper.style.position = 'fixed';
+            indicatorWrapper.style.zIndex = '9999';
+            indicatorWrapper.style.textAlign = 'center';
+            indicatorWrapper.id = 'indicator-wrapper';
+            document.body.appendChild(indicatorWrapper);
+
             var scollLengthIndicator = document.createElement('div');
-            document.body.appendChild(scollLengthIndicator);
-            scollLengthIndicator.style.right = '32px';
-            scollLengthIndicator.style.top = '10px';
-            scollLengthIndicator.style.position = 'fixed';
-            scollLengthIndicator.style.zIndex = '9999';
-            scollLengthIndicator.style.fontSize = '20px';
+            scollLengthIndicator.style.fontSize = '18px';
             scollLengthIndicator.style.fontWeight = 'bold';
             scollLengthIndicator.style.color = 'red';
-            scollLengthIndicator.id = 'scrolled-km';
+            scollLengthIndicator.id = 'indicator';
+            scollLengthIndicator.textContent = ((Number(response.scrolled) / Number(dpi_y)) * 0.0254).toFixed(1) + " Meters";
+            document.getElementById("indicator-wrapper").appendChild(scollLengthIndicator);
+
+            $("<span id='real-life-height' style='color:white;font-weight:bold;'></span>").insertAfter("#indicator");
 
             var start;
             var end;
             var scrolledPixelsCount;
+
+            var realWorldHeights = {
+                26: "Big Ben",
+                30: "Eifel tower",
+                32: "Angel Waterfall",
+            };
+
+            var lastRealWorldHeight = "Nothing";
 
             $(window)
                     .on("scrollstart", function () {
@@ -49,9 +65,19 @@ var readyStateCheckInterval = setInterval(function () {
                         chrome.runtime.sendMessage({status: "start"},
                         function (response) {
                             var allScolledPixels = scrolledPixelsCount + Number(response.scrolled);
+                            var meters = ((allScolledPixels / Number(dpi_y)) * 0.0254).toFixed(1);
 
-                            var doc = document.documentElement;
-                            document.getElementById("scrolled-km").innerText = ((allScolledPixels / Number(dpi_y)) * 0.0254).toFixed(1) + " Meters";
+                            document.getElementById("indicator").innerText = meters + " Meters";
+
+                            if (Math.round(meters) in realWorldHeights)
+                            {
+                                lastRealWorldHeight = realWorldHeights[Math.round(meters)]
+                                document.getElementById("real-life-height").innerText = lastRealWorldHeight;
+                            } else
+                            {
+                                document.getElementById("real-life-height").innerText = "Passed " + lastRealWorldHeight;
+                            }
+
                             chrome.runtime.sendMessage({scrolledPixels: allScolledPixels},
                             function (response) {
                                 //console.log(response.farewell); 
